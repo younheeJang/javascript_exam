530539c461aeb2f6ae003f3498a208937a33bd45
younheeJang <edam7635@gmail.com>
2021-06-02 19:05:24 +0900

    prac.joke-teller:joke and voice api

--- prac.joke-teller/script.js
+++ prac.joke-teller/script.js
@@ -1,82 +1,46 @@
-//
-const imageContainer = document.getElementById('image-container');
-const loader = document.getElementById('loader');
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
 
-let ready = false;
-let imagesLoaded = 0;
-let totalImages = 0;
-let photosArray = [];
-let initialLoad = true;
-//unsplash api:
-const count = 5;
-const apiKey = ``;
-let apiUrl = `https://api/.unsplash.com/photos/random/?client_id=${apiKey}$count=${count}`;
-
-//check if all images were loaded:
-function imageLoaded() {
-  imagesLoaded++;
-  if (imagesLoaded === totalImages) {
-    ready = true;
-    loader.hidden = true;
-    count = 30;
-  }
+//disable enable button:
+function toggleButton() {
+  button.disabled = !button.disabled;
 }
 
-//helper setAttribute
-function setAttributes(element, attributes) {
-  for (const key in attributes) {
-    element.setAttribute(key, attributes[key]);
-  }
-}
-
-//create element and add to dom
-function displayPhotos() {
-  imagesLoaded = 0;
-  totalImages = photosArray.length;
-
-  photosArray.forEach(photo => {
-    //create <a>
-    const item = document.createElement('a');
-
-    setAttributes(item, {
-      href: photo.links.html,
-      target: '_blank'
-    });
-    //create <img>
-    const img = document.createElement('img');
-
-    setAttributes(img, {
-      src: photo.urls.regular,
-      alt: photo.alt_description,
-      title: photo.alt_description
-    });
-
-    //check when each is finished loading:
-    img.addEventListener('load', imageLoaded);
-    //put img -> a
-    item.appendChild(img);
-    imageContainer.appendChild(item);
+//passing joke to voicerss api:
+function tellMe(joke) {
+  console.log('tell me: ', joke);
+  VoiceRSS.speech({
+    key: 'your app key',
+    src: joke,
+    hl: 'en-us',
+    v: 'Linda',
+    r: 0,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false
   });
 }
 
-//get photos from unsplash api:
-async function getPhotos() {
+//get jokes:
+async function getJokes() {
+  let joke = '';
+  const apiUrl =
+    'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
   try {
     const response = await fetch(apiUrl);
-    const photosArray = await response.json();
-  } catch (err) {}
+    const data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup}...${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    tellMe(joke);
+    toggleButton();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-//scroll near bottom, load more photos:
-window.addEventListener('scroll', () => {
-  if (
-    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
-    ready
-  ) {
-    ready = false;
-    getPhotos();
-  }
-});
-
-//on load
-getPhotos();
+//event listeners:
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
