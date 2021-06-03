9cffab6bd2b47ac2e758b203868266cec7c4f598
younheeJang <edam7635@gmail.com>
2021-06-03 17:44:47 +0900

    prac.data-structure:linked-list>reverse

--- prac.data-structure/linked-list.js
+++ prac.data-structure/linked-list.js
@@ -81,6 +81,24 @@ class LinkedList {
     this.length--;
     return this.printList();
   }
+
+  reverse() {
+    if (!this.head.next) {
+      return this.head;
+    }
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
