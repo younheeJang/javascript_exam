212e5e5a8747a79c9a8df1adbe16613240c08550
younheeJang <edam7635@gmail.com>
2021-06-03 17:32:57 +0900

    prac.data-structure: doubly-linked-list

--- prac.data-structure/doubly-linked-list.js
+++ prac.data-structure/doubly-linked-list.js
@@ -5,11 +5,12 @@
 //   }
 // }
 
-class LinkedList {
+class DoublyLinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      next: null
+      next: null,
+      prev: null
     };
     this.tail = this.head;
     this.length = 1;
@@ -19,8 +20,10 @@ class LinkedList {
     //const newNode = new Node(value);
     const newNode = {
       value: value,
-      next: null
+      next: null,
+      prev: null
     };
+    newNode.prev = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
@@ -30,9 +33,11 @@ class LinkedList {
   prepend(value) {
     const newNode = {
       value: value,
-      next: null
+      next: null,
+      prev: null
     };
     newNode.next = this.head;
+    this.head.prev = newNode;
     this.head = newNode;
     this.length++;
     return this;
@@ -54,12 +59,15 @@ class LinkedList {
     }
     const newNode = {
       value: value,
-      next: null
+      next: null,
+      prev: null
     };
     const leader = this.traverseToIndex(index - 1);
-    const holdingPointer = leader.next;
+    const follwer = leader.next;
     leader.next = newNode;
-    newNode.next = holdingPointer;
+    newNode.prev = leader;
+    newNode.next = follwer;
+    follwer.prev = newNode;
     this.length++;
     return this.printList();
   }
@@ -83,4 +91,4 @@ class LinkedList {
   }
 }
 
-const myLinkedList = new LinkedList(10);
+const myDLinkedList = new DoublyLinkedList(10);
