464da6e7f876161b70476b5809345282ff06eb0f
younheeJang <edam7635@gmail.com>
2021-06-05 23:18:03 +0900

    prac.data-structure: create stack.js

---
+++ prac.data-structure/stack.js
@@ -0,0 +1,22 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek() {}
+
+  push() {}
+
+  pop() {}
+}
+
+const myStack = new Stack();
