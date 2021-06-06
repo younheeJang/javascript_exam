fc02523834b323e7b425587efe99d43a7c6ae55f
younheeJang <edam7635@gmail.com>
2021-06-06 14:18:49 +0900

    prac.data-structure: stack with linked-list conception

--- prac.data-structure/stack.js
+++ prac.data-structure/stack.js
@@ -12,11 +12,36 @@ class Stack {
     this.length = 0;
   }
 
-  peek() {}
+  peek() {
+    return this.top;
+  }
 
-  push() {}
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
 
-  pop() {}
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return holdingPointer;
+  }
 }
 
 const myStack = new Stack();
