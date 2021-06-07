cffbeee48ed53b4e8d45e6e118cb52d6577abf21
younheeJang <edam7635@gmail.com>
2021-06-07 17:34:24 +0900

    prac.data-structure: binary-search-tree + insert, lookup

--- prac.data-structure/binary-search-tree.js
+++ prac.data-structure/binary-search-tree.js
@@ -10,8 +10,46 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  insert(value) {}
-  lookup(value) {}
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          //left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return false;
+  }
 }
 
 function traverse(node) {
