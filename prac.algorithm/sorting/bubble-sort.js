d92d344c1e517040ca175314e5aa26bf26a9b6aa
younheeJang <edam7635@gmail.com>
2021-06-10 20:11:45 +0900

    prac.algorithm: sorting/bubble-sort.js

---
+++ prac.algorithm/sorting/bubble-sort.js
@@ -0,0 +1,15 @@
+const numbers = [9, 3, 2, 1];
+
+function bubbleSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (array[j] > array[j + 1]) {
+        //swap
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+}
